efee22122859b929b43da69ae11108f38c13c734
Ephraim19 <machayoephraim@gmail.com>
2022-02-20 10:20:21 +0300

    Created decoding functonality and the frontend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import models.Caesar;
+
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -7,7 +9,14 @@ public class App {
      System.out.println("Enter your word");
      String myWord = myConsole.nextLine();
      Caesar caesar = new Caesar(myWord);
-     String wordResult = caesar.encodeLetters();
+     System.out.println("Do you want to decode or encode");
+     String functionality = myConsole.nextLine();
+     String wordResult;
+     if (Objects.equals(functionality, "encode")){
+         wordResult = caesar.encodeLetters();
+     }else {
+         wordResult = caesar.decodeLetters();
+     }
      System.out.println(wordResult);
     }
 }
--- src/main/java/models/Caesar.java
+++ src/main/java/models/Caesar.java
@@ -25,6 +25,24 @@ public class Caesar {
             char newLetter = allLeters[newIndex];
             newWord = letters.replace(firstLetter, newLetter);
             letters = newWord;
+            System.out.println(letters);
+        }
+        return letters;
+    }
+
+    public String decodeLetters() {
+        char[] userLetters = letters.toCharArray();
+        char[] allLeters = "abcdefghijklmnopqrstvuwxyz".toCharArray();
+        int i = 0;
+        String newWord = null;
+        for (i = 0; i < userLetters.length; i++) {
+            char firstLetter = userLetters[i];
+            int originalIndex = "abcdefghijklmnopqrstvuwxyz".indexOf(firstLetter);
+            //System.out.println(originalIndex);
+            int newIndex = originalIndex + 3;
+            char newLetter = allLeters[newIndex];
+            newWord = letters.replace(firstLetter, newLetter);
+            letters = newWord;
         }
         return newWord;
     }
